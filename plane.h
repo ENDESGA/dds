2ee379bee3180e4ef504891f8ca1f8163d8c349f
ENDESGA <38831818+ENDESGA@users.noreply.github.com>
2022-07-22 23:20:02 +1200

    Update plane.h

--- plane.h
+++ plane.h
@@ -47,10 +47,11 @@ plane _new_plane( uint type_size )
 	p.size_mem_11 = 1, p.size_mem_10 = 1, p.size_mem_00 = 1, p.size_mem_01 = 1;
 	p.size_type = type_size;
 	//
-	p.data_11 = (dataptr)malloc( p.size_type);
-	p.data_10 = (dataptr)malloc( p.size_type);
-	p.data_00 = (dataptr)malloc( p.size_type);
-	p.data_01 = (dataptr)malloc( p.size_type);
+	p.data_11 = (dataptr)calloc( 1, p.size_type );
+	p.data_10 = (dataptr)calloc( 1, p.size_type );
+	p.data_00 = (dataptr)calloc( 1, p.size_type );
+	p.data_01 = (dataptr)calloc( 1, p.size_type );
+
 	return p;
 }
 
@@ -61,71 +62,76 @@ plane _new_plane( uint type_size )
 // x - horizonal position
 // y - vertical position
 
-#define plane_get( p, t, x, y ) ( ( x >= 0 ) ? ( ( y >= 0 ) ? ( ( (tile*)p.data_11 )[ ( x ) + ( y * p.width_11 ) ] ) : ( ( (tile*)p.data_10 )[ ( x ) + ( ( -y - 1 ) * p.width_10 ) ] ) ) : ( ( y < 0 ) ? ( ( (tile*)p.data_00 )[ ( -x - 1 ) + ( ( -y - 1 ) * p.width_00 ) ] ) : ( ( (tile*)p.data_01 )[ ( -x - 1 ) + ( (y)*p.width_01 ) ] ) ) )
+#define plane_at( p, t, x, y ) ( ( x >= 0 ) ?                                                                 \
+																	 ( ( y >= 0 ) ?                                                             \
+																			 ( ( (tile*)p.data_11 )[ ( x ) + ( y * p.width_11 ) ] ) :               \
+																			 ( ( (tile*)p.data_10 )[ ( x ) + ( ( -y - 1 ) * p.width_10 ) ] ) ) :    \
+																	 ( ( y < 0 ) ?                                                              \
+																			 ( ( (tile*)p.data_00 )[ ( -x - 1 ) + ( ( -y - 1 ) * p.width_00 ) ] ) : \
+																			 ( ( (tile*)p.data_01 )[ ( -x - 1 ) + ( y * p.width_01 ) ] ) ) )
+
+#define plane_get( p, t, x, y ) ( ( x >= 0 ) ?                                                                       \
+																		( ( y >= 0 ) ?                                                                   \
+																				( ( x >= p.width_11 || y >= p.height_11 ) ?                                  \
+																						( ( t ){ 0 } ) :                                                         \
+																						( ( (tile*)p.data_11 )[ ( x ) + ( y * p.width_11 ) ] ) ) :               \
+																				( ( x >= p.width_10 || ( -y - 1 ) >= p.height_10 ) ?                         \
+																						( ( t ){ 0 } ) :                                                         \
+																						( ( (tile*)p.data_10 )[ ( x ) + ( ( -y - 1 ) * p.width_10 ) ] ) ) ) :    \
+																		( ( y < 0 ) ?                                                                    \
+																				( ( ( -x - 1 ) >= p.width_00 || ( -y - 1 ) >= p.height_00 ) ?                \
+																						( ( t ){ 0 } ) :                                                         \
+																						( ( (tile*)p.data_00 )[ ( -x - 1 ) + ( ( -y - 1 ) * p.width_00 ) ] ) ) : \
+																				( ( ( -x - 1 ) >= p.width_01 || y >= p.height_01 ) ?                         \
+																						( ( t ){ 0 } ) :                                                         \
+																						( ( (tile*)p.data_01 )[ ( -x - 1 ) + ( y * p.width_01 ) ] ) ) ) )
 
 //
 
-#define plane_set( p, t, v, x, y )                                                                     \
-	do {                                                                                                 \
-		if( x >= 0 )                                                                                       \
-		{                                                                                                  \
-			if( y >= 0 )                                                                                     \
-			{                                                                                                \
-				p.width_11 = ( ( x < p.width_11 ) ? ( p.width_11 ) : ( x + 1 ) );                              \
-				p.height_11 = ( ( y < p.height_11 ) ? ( p.height_11 ) : ( y + 1 ) );                           \
-				p.area_11 = p.width_11 * p.height_11;                                                          \
-				p.count_11++;                                                                                  \
-                                                                                                       \
-				if( p.area_11 >= p.size_mem_11 )                                                               \
-				{                                                                                              \
-					p.size_mem_11 = ( ( p.area_11 == p.size_mem_11 ) ? ( p.size_mem_11 << 1 ) : ( p.area_11 ) ); \
-					p.data_11 = (dataptr)realloc( p.data_11, p.size_mem_11 * p.size_type );                      \
-				}                                                                                              \
-				( (t*)( p.data_11 ) )[ ( x ) + ( y * p.width_11 ) ] = v;                                       \
-			} else                                                                                           \
-			{                                                                                                \
-				p.width_10 = ( ( x < p.width_10 ) ? ( p.width_10 ) : ( x + 1 ) );                              \
-				p.height_10 = ( ( y < p.height_10 ) ? ( p.height_10 ) : ( -y ) );                              \
-				p.area_10 = p.width_10 * p.height_10;                                                          \
-				p.count_10++;                                                                                  \
-                                                                                                       \
-				if( p.area_10 >= p.size_mem_10 )                                                               \
-				{                                                                                              \
-					p.size_mem_10 = ( ( p.area_10 == p.size_mem_10 ) ? ( p.size_mem_10 << 1 ) : ( p.area_10 ) ); \
-					p.data_10 = (dataptr)realloc( p.data_10, p.size_mem_10 * p.size_type );                      \
-				}                                                                                              \
-				( (t*)( p.data_10 ) )[ ( x ) + ( ( -y - 1 ) * p.width_10 ) ] = v;                              \
-			}                                                                                                \
-		} else                                                                                             \
-		{                                                                                                  \
-			if( y < 0 )                                                                                      \
-			{                                                                                                \
-				p.width_00 = ( ( ( -x - 1 ) < p.width_00 ) ? ( p.width_00 ) : ( -x ) );                        \
-				p.height_00 = ( ( ( -y - 1 ) < p.height_00 ) ? ( p.height_00 ) : ( -y ) );                     \
-				p.area_00 = p.width_00 * p.height_00;                                                          \
-				p.count_00++;                                                                                  \
-                                                                                                       \
-				if( p.area_00 >= p.size_mem_00 )                                                               \
-				{                                                                                              \
-					p.size_mem_00 = ( ( p.area_00 == p.size_mem_00 ) ? ( p.size_mem_00 << 1 ) : ( p.area_00 ) ); \
-					p.data_00 = (dataptr)realloc( p.data_00, p.size_mem_00 * p.size_type );                      \
-				}                                                                                              \
-				( (t*)( p.data_00 ) )[ ( ( -x - 1 ) ) + ( ( -y - 1 ) * p.width_00 ) ] = v;                     \
-			} else                                                                                           \
-			{                                                                                                \
-				p.width_01 = ( ( ( -x - 1 ) < p.width_01 ) ? ( p.width_01 ) : ( -x ) );                        \
-				p.height_01 = ( ( y < p.height_01 ) ? ( p.height_01 ) : ( y + 1 ) );                           \
-				p.area_01 = p.width_01 * p.height_01;                                                          \
-				p.count_01++;                                                                                  \
-                                                                                                       \
-				if( p.area_01 >= p.size_mem_00 )                                                               \
-				{                                                                                              \
-					p.size_mem_01 = ( ( p.area_01 == p.size_mem_00 ) ? ( p.size_mem_00 << 1 ) : ( p.area_01 ) ); \
-					p.data_01 = (dataptr)realloc( p.data_01, p.size_mem_01 * p.size_type );                      \
-				}                                                                                              \
-				( (t*)( p.data_01 ) )[ ( ( -x - 1 ) ) + ( y * p.width_01 ) ] = v;                              \
-			}                                                                                                \
-		}                                                                                                  \
+#define _plane_apply( p, t, v, x, y, plane_id )                                                                                                                                                                \
+	do {                                                                                                                                                                                                         \
+		int __X = ( x ), __Y = ( y );                                                                                                                                                                              \
+		uint temp_width_##plane_id = p.width_##plane_id, temp_height_##plane_id = p.height_##plane_id;                                                                                                             \
+		p.width_##plane_id = ( ( __X < p.width_##plane_id ) ? ( p.width_##plane_id ) : ( __X + 1 ) );                                                                                                              \
+		p.height_##plane_id = ( ( __Y < p.height_##plane_id ) ? ( p.height_##plane_id ) : ( __Y + 1 ) );                                                                                                           \
+		p.area_##plane_id = p.width_##plane_id * p.height_##plane_id;                                                                                                                                              \
+		p.count_##plane_id++;                                                                                                                                                                                      \
+                                                                                                                                                                                                               \
+		if( p.area_##plane_id > p.size_mem_##plane_id )                                                                                                                                                            \
+		{                                                                                                                                                                                                          \
+			p.size_mem_##plane_id = p.area_##plane_id;                                                                                                                                                               \
+			dataptr temp_data_##plane_id = (dataptr)calloc( p.size_mem_##plane_id, p.size_type );                                                                                                                    \
+			for( uint m_##plane_id = 0; m_##plane_id < temp_height_##plane_id; ++m_##plane_id )                                                                                                                      \
+			{                                                                                                                                                                                                        \
+				memcpy( temp_data_##plane_id + ( p.width_##plane_id * m_##plane_id * p.size_type ), p.data_##plane_id + ( temp_width_##plane_id * m_##plane_id * p.size_type ), temp_width_##plane_id * p.size_type ); \
+			}                                                                                                                                                                                                        \
+			p.data_##plane_id = temp_data_##plane_id;                                                                                                                                                                \
+		}                                                                                                                                                                                                          \
+		( (t*)( p.data_##plane_id ) )[ __X + ( __Y * p.width_##plane_id ) ] = v;                                                                                                                                   \
+	} while( 0 )
+
+#define plane_set( p, t, v, x, y )                     \
+	do {                                                 \
+		int _X = ( x ), _Y = ( y );                        \
+		if( ( _X ) >= 0 )                                  \
+		{                                                  \
+			if( ( _Y ) >= 0 )                                \
+			{                                                \
+				_plane_apply( p, t, v, _X, _Y, 11 );           \
+			} else                                           \
+			{                                                \
+				_plane_apply( p, t, v, _X, -_Y - 1, 10 );      \
+			}                                                \
+		} else                                             \
+		{                                                  \
+			if( ( _Y ) < 0 )                                 \
+			{                                                \
+				_plane_apply( p, t, v, -_X - 1, -_Y - 1, 00 ); \
+			} else                                           \
+			{                                                \
+				_plane_apply( p, t, v, -_X - 1, _Y, 01 );      \
+			}                                                \
+		}                                                  \
 	} while( 0 )
 
 //
